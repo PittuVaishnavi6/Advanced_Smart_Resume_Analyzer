7b16ed0efeeb7ff028cae60f8ebdaa0a2c63ef6c
PittuVaishnavi6 <vaishnavipittu20@gmail.com>
2025-07-24 16:36:57 +0530

    Initial commit - Smart Resume Analyzer

---
+++ src/test/java/com/example/smartresume/ResumeAnalyzerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.smartresume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeAnalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
